c66df826127318560735943c1f4c29e1fd5d0166
Camila Matias <camila.matias@zappts.com.br>
2024-04-04 15:25:11 -0300

    feat: iniciar a lógica da validação do formulário

---
+++ scripts/formulario.js
@@ -0,0 +1,34 @@
+const inputNome = document.getElementById("nome");
+const inputEmail = document.getElementById("email");
+const inputComentario = document.getElementById("comentario");
+const erroNome = document.getElementById("erro-nome");
+const erroEmail = document.getElementById("erro-email");
+const erroComentario = document.getElementById("erro-comentario");
+const botaoEnviarComentario = document.getElementById("botao-enviar-comentario");
+
+document.getElementById("formulario").addEventListener('submit', (evento) => {
+    console.log(evento);
+    let contadorErros = 0;
+    if (inputNome.value === "") {
+        erroNome.style.display = "block";
+    } else {
+        contadorErros++;
+    }
+
+    if (inputEmail.value === "" || inputEmail.value !== "/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g") {
+        erroEmail.style.display = "block";
+    } else {
+        contadorErros++;
+    }
+
+    if (inputComentario.value === "") {
+        erroComentario.style.marginTop = "-0.8rem";
+        erroComentario.style.display = "block";
+    } else {
+        contadorErros++;
+    }
+
+    if (contadorErros > 0) {
+        evento.preventDefault();
+    }
+}) 
