c66e44e1c6578e933364f071d23f0715301a1c97
Camila Matias <camila.matias@zappts.com.br>
2024-04-05 14:50:46 -0300

    feat: finalizar validação formulário

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -10,16 +10,16 @@ const botaoEnviarComentario = document.getElementById("botao-enviar-comentario")
 const inputsFormulario = [inputNome, inputEmail, inputComentario];
 inputsFormulario.forEach((input) => {
     input.addEventListener('input', habilitacaoBotao);
-    input.addEventListener('blur', evento => verificaInput(evento));
+    input.addEventListener('input', evento => verificaInput(evento));
 });
 
 function verificaInput(evento) {
-    const regExEmail1 = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+).(\.[a-z]{2,3})$/g;
-    const regExEmail2 = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/g;
+    const regExEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    const regExVazio = /^(?=\s*$)/;
 
     switch (evento.target.id) {
         case "nome":
-            if (inputNome.value === "") {
+            if (inputNome.value === "" || regExVazio.test(inputNome.value)) {
                 erroNome.style.display = "block";
                 if (window.screen.availWidth < 767) {
                     erroNome.style.marginTop = "-1.2rem";
@@ -30,14 +30,14 @@ function verificaInput(evento) {
             }
             break;
         case "email":
-            if (inputEmail.value === "" || (!regExEmail1.test((inputEmail.value)) || !regExEmail2.test((inputEmail.value)))){
+            if (inputEmail.value === "" || !regExEmail.test((inputEmail.value))){
                 erroEmail.style.display = "block";
             } else {
                 erroEmail.style.display = "none";
             }
             break;
         case "comentario":
-            if (inputComentario.value === "") {
+            if (inputComentario.value === "" || regExVazio.test(inputComentario.value)) {
                 erroComentario.style.marginTop = "-0.8rem";
                 erroComentario.style.display = "block";
             } else {
@@ -49,11 +49,6 @@ function verificaInput(evento) {
             break;       
     }
 }
-
-//Validação do formulário
-document.getElementById("formulario-comentario").addEventListener('submit', (evento) => {
-
-});
 
 //Clique no checkbox
 const inputCheckbox = document.getElementById("salvar-info");
@@ -73,9 +68,9 @@ imagemCheckbox.addEventListener('click', (evento) => {
 
 //Enable/disable do botão de enviar
 function habilitacaoBotao() {
-    const validacaoDisplayErros = erroNome.style.display === "none" && erroEmail.style.display === "none" && erroComentario.style.display === "none";
+    //const validacaoDisplayErros = erroNome.style.display === "none" && erroEmail.style.display === "none" && erroComentario.style.display === "none";
     const validacaoCamposVazios = inputNome.value !== "" && inputEmail.value !== "" && inputComentario.value !== "";
-    if (validacaoCamposVazios && validacaoDisplayErros) {
+    if (validacaoCamposVazios) {
         botaoEnviarComentario.classList.add("botao__primario--enabled");
         botaoEnviarComentario.removeAttribute("disabled", "");
     } else {
