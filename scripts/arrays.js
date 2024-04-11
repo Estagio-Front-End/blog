8d62985bb50d67dacf0439c2b6411d8143732dcb
Camila Matias <camila.matias@zappts.com.br>
2024-04-11 16:05:06 -0300

    feat: adicionar exemplos de métodos de edição

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,3 +1,4 @@
+//Definições de array
 let arrayHomogeneo = JSON.stringify([1, 2, 3, 4, 5]);
 let arrayHeterogeneo = JSON.stringify([1, true, "Camila", {nome: "Camila", idade: 29}, [6, 7, 8]]);
 
@@ -20,3 +21,57 @@ function mostraResultado() {
 
 const exemploArrayObjetos = document.getElementById("array-objetos");
 exemploArrayObjetos.innerText += ` ${JSON.stringify([{nome: "Camila", cargo: "Estagiária"},{nome: "Valéria", cargo: "Estagiária"},{nome: "Júlia", cargo: "Estagiária"}])}`;
+
+//Métodos de edição 
+var arrayInicial = [1,2,3,4,5,6,7];
+document.getElementById('array-inicial').innerText += ` ${JSON.stringify(arrayInicial)}`;
+const resultadoBusca = document.getElementById('resultado-edicao');
+
+function buscaArray(metodo) {
+    var resultado;
+    switch (metodo) {
+        case "foreach":
+            resultadoBusca.innerText = 'O resultado do método é:';
+            arrayInicial.forEach((numero, index) => resultadoBusca.innerHTML += `<p>O número ${index+1} é ${numero}; </p>`);   
+            break;
+        case "push":
+            resultado = arrayInicial.push(8,9);
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "pop":
+            resultado = arrayInicial.pop();
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "shift":
+            resultado = arrayInicial.shift();
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "unshift":
+            resultado = arrayInicial.unshift(8,9);
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "sort":
+            resultado = arrayInicial.sort();
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "fill":
+            resultado = arrayInicial.fill(10, 2, 4);
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "reverse":
+            resultado = arrayInicial.reverse();
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "splice":
+            resultado = arrayInicial.splice(0, 3, 25);
+            resultadoBusca.innerText = `O resultado do método é: ${arrayInicial}`;    
+            break;
+        case "reduce":
+            resultado = arrayInicial.reduce((acumulador, valor) => acumulador + valor, 0);
+            resultadoBusca.innerText = `O resultado do método é: ${resultado}`;    
+            break;
+        default:
+            resultadoBusca.innerText = "";   
+            break;
+    }
+}
