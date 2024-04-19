b83b7d4391cebe4f2b0dc4cab96034570bd285df
Camila Matias <camila.matias@zappts.com.br>
2024-04-19 15:43:37 -0300

    feat: criar máscaras para cpf

--- scripts/regex.js
+++ scripts/regex.js
@@ -1,9 +1,11 @@
-const regexCPF = /\d{3}[\.]\d{3}[\.]\d{3}[\-]\d{2}/gis;
+const regexCPF = /(\d{3})(\d{3})(\d{3})(\d{2})/gis;
 const regexCNPJ = /\d{2}[\.]\d{3}[\.]\d{3}[\/](0001|0002)[\-]\d{2}/gis;
 const regexDataNascimento = /\d{2}[\/]\d{2}[\/]\d{4}/gis
 const regexReal = /[\R\$]\d+[\,]\d{2}/gis
 const regexEmail = /[a-z0-9]+[\-]?[\_]?[a-z0-9]+[\@][a-z]+[\.][a-z]+([\.][a-z]+)?/gis
 const regexCelular = /[\(]\d{2}[\)][9]\d{4}[\-]\d{4}/gis
+
+const regexOrdinarioCPF = /(\d{3})[\.](\d{3})[\.](\d{3})[\-](\d{2})/gis;
 
 const inputCPF = document.getElementById("cpf-regex");
 const inputCNPJ = document.getElementById("cnpj-regex");
@@ -15,14 +17,19 @@ const inputCelular = document.getElementById("celular-regex");
 let campos = [inputCPF, inputCNPJ, inputData, inputReal, inputEmailRegex, inputCelular];
 let padroes = [regexCPF, regexCNPJ, regexDataNascimento, regexReal, regexEmail, regexCelular];
 
-campos.forEach(campo => campo.addEventListener('blur', evento => colocarMascara(evento)))
+inputCPF.addEventListener('focus', evento => mascaraVazia(evento))
+inputCPF.addEventListener('blur', evento => mascaraCPF(evento))
 
-function colocarMascara(evento) {
-    campos.forEach(campo => { 
-        evento.target === campo 
-        ? campo.value.replace(padroes[campos.indexOf(campo)]) 
-        : campo.value = campo.value
-    })
+function mascaraVazia(evento) {
+    console.log("dentro")
+    console.log(inputCPF.value);
+    console.log(evento.target.value.replace(regexOrdinario, "$1$2$3$4"))
+    inputCPF.value = evento.target.value.replace(regexOrdinario, "$1$2$3$4");
+}
+
+function mascaraCPF(evento){
+    console.log("fora")
+    inputCPF.value = evento.target.value.replace(regexCPF, "$1.$2.$3-$4");
 }
 
 const botaoRegex = document.getElementById("botao-regex");
