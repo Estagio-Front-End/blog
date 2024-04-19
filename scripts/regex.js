c4b4ef5989256aace917aa3ba944ac7cb9a7d967
Camila Matias <camila.matias@zappts.com.br>
2024-04-19 15:18:16 -0300

    feat: tentar criar máscaras dos campos

--- scripts/regex.js
+++ scripts/regex.js
@@ -1,9 +1,9 @@
-const regexCPF = /\d{3}[\.]\d{3}[\.]\d{3}[\-]\d{2}/gi;
-const regexCNPJ = /\d{2}[\.]\d{3}[\.]\d{3}[\/](0001|0002)[\-]\d{2}/gi;
-const regexDataNascimento = /\d{2}[\/]\d{2}[\/]\d{4}/gi
-const regexReal = /[\R\$]\d+[\,]\d{2}/gi
-const regexEmail = /[a-z0-9]+[\@][a-z]+[\.][a-z]+([\.][a-z]+)?/gi
-const regexCelular = /[\(]\d{2}[\)][9]\d{4}[\-]\d{4}/gi
+const regexCPF = /\d{3}[\.]\d{3}[\.]\d{3}[\-]\d{2}/gis;
+const regexCNPJ = /\d{2}[\.]\d{3}[\.]\d{3}[\/](0001|0002)[\-]\d{2}/gis;
+const regexDataNascimento = /\d{2}[\/]\d{2}[\/]\d{4}/gis
+const regexReal = /[\R\$]\d+[\,]\d{2}/gis
+const regexEmail = /[a-z0-9]+[\-]?[\_]?[a-z0-9]+[\@][a-z]+[\.][a-z]+([\.][a-z]+)?/gis
+const regexCelular = /[\(]\d{2}[\)][9]\d{4}[\-]\d{4}/gis
 
 const inputCPF = document.getElementById("cpf-regex");
 const inputCNPJ = document.getElementById("cnpj-regex");
@@ -11,6 +11,19 @@ const inputData = document.getElementById("data-regex");
 const inputReal = document.getElementById("monetario-regex");
 const inputEmailRegex = document.getElementById("email-regex");
 const inputCelular = document.getElementById("celular-regex");
+
+let campos = [inputCPF, inputCNPJ, inputData, inputReal, inputEmailRegex, inputCelular];
+let padroes = [regexCPF, regexCNPJ, regexDataNascimento, regexReal, regexEmail, regexCelular];
+
+campos.forEach(campo => campo.addEventListener('blur', evento => colocarMascara(evento)))
+
+function colocarMascara(evento) {
+    campos.forEach(campo => { 
+        evento.target === campo 
+        ? campo.value.replace(padroes[campos.indexOf(campo)]) 
+        : campo.value = campo.value
+    })
+}
 
 const botaoRegex = document.getElementById("botao-regex");
 
@@ -33,7 +46,6 @@ function validaCampos(evento) {
 
 function retornaResultado(isCPF, isCNPJ, isData, isReal, isEmail, isCelular, validaVazio) {
     let valores = [isCPF, isCNPJ, isData, isReal, isEmail, isCelular];
-    let campos = [inputCPF, inputCNPJ, inputData, inputReal, inputEmailRegex, inputCelular];
     let resultadoRegex = document.getElementById("resultado-regex");
 
     if(validaVazio) {
