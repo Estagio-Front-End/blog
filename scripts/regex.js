9ad06b3010467e82b15f0ec0095bba1f5f5d97b9
Camila Matias <camila.matias@zappts.com.br>
2024-04-22 09:57:49 -0300

    feat: adicionar funções de colocar e retirar máscara

--- scripts/regex.js
+++ scripts/regex.js
@@ -1,11 +1,11 @@
 const regexCPF = /(\d{3})(\d{3})(\d{3})(\d{2})/gis;
 const regexCNPJ = /(\d{2})(\d{3})(\d{3})(0001|0002)(\d{2})/gis;
-const regexDataNascimento = /\d{2}[\/]\d{2}[\/]\d{4}/gis
-const regexReal = /[\R\$]\d+[\,]\d{2}/gis
+const regexDataNascimento = /(\d{2})(\d{2})(\d{4})/gis
+const regexReal = /(\d+)(\,)(\d{2})/gis
 const regexEmail = /[a-z0-9]+[\-]?[\_]?[a-z0-9]+[\@][a-z]+[\.][a-z]+([\.][a-z]+)?/gis
-const regexCelular = /[\(]\d{2}[\)][9]\d{4}[\-]\d{4}/gis
+const regexCelular = /(\d{2})([9])(\d{4})(\d{4})/gis
 
-const regexOrdinario = /[.-]|[(]|[)]|[/]/gis;
+const regexOrdinario = /[.-]|[(]|[)]|[/]|[R]|[$]|[\s]/gis;
 
 const inputCPF = document.getElementById("cpf-regex");
 const inputCNPJ = document.getElementById("cnpj-regex");
@@ -17,24 +17,46 @@ const inputCelular = document.getElementById("celular-regex");
 let campos = [inputCPF, inputCNPJ, inputData, inputReal, inputEmailRegex, inputCelular];
 let padroes = [regexCPF, regexCNPJ, regexDataNascimento, regexReal, regexEmail, regexCelular];
 
-inputCPF.addEventListener('focus', evento => tirarMascara(evento))
-inputCPF.addEventListener('blur', evento => mascaraCPF(evento))
-
-inputCNPJ.addEventListener('focus', evento => tirarMascara(evento))
-inputCNPJ.addEventListener('blur', evento => mascaraCNPJ(evento))
-
-//Tem que ser uma função desse jeito, mas todas as máscaras numa função só com switch porque se não quando clicar em uma a outra some :(
+campos.forEach(campo => campo.addEventListener('focus', evento => tirarMascara(evento)))
+campos.forEach(campo => campo.addEventListener('blur', evento => colocarMascara(evento)))
 
 function tirarMascara(evento) {
-    inputCPF.value = evento.target.value.replace(regexOrdinario, "");
+    let input = evento.target;
+    if (input === inputCPF || input === inputCNPJ || input === inputData || input === inputReal || input === inputCelular) {
+        input.value = evento.target.value.replace(regexOrdinario, "");
+    } else if (input === inputEmailRegex) {
+        input.value = evento.target.value;
+    } else {
+        alert("Ocorreu um erro, tente novamente mais tarde.");
+    } 
 }
 
-function mascaraCPF(evento){
-    inputCPF.value = evento.target.value.replace(regexCPF, "$1.$2.$3-$4");
+function colocarMascara(evento){
+    switch (evento.target) {
+        case inputCPF:
+            inputCPF.value = evento.target.value.replace(regexCPF, "$1.$2.$3-$4");
+        break;
+        case inputCNPJ:
+            inputCNPJ.value = evento.target.value.replace(regexCNPJ, "$1.$2.$3/$4-$5");
+        break;
+        case inputData:
+            inputData.value = evento.target.value.replace(regexDataNascimento, "$1/$2/$3");
+        break;
+        case inputReal:
+            inputReal.value = evento.target.value.replace(regexReal, "R$ $1$2$3");
+        break;
+        case inputEmailRegex:
+            inputEmailRegex.value = evento.target.value;
+        break;
+        case inputCelular:
+            inputCelular.value = evento.target.value.replace(regexCelular, "($1) $2$3-$4");
+        break;
+
+    }
 }
 
+//Validação dos campos com Regex
 const botaoRegex = document.getElementById("botao-regex");
-
 botaoRegex.addEventListener('click', evento => validaCampos(evento));
 
 function validaCampos(evento) {
