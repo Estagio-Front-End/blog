518fc4421a6bee764156ef46510e78adf21e75f4
Camila Matias <camila.matias@zappts.com.br>
2024-04-25 10:02:16 -0300

    feat: finalizar conteúdo e exemplo de css no shadow DOM

--- scripts/shadow-dom.js
+++ scripts/shadow-dom.js
@@ -17,21 +17,31 @@ botaoShadow.innerText = "Aumentar tamanho de fonte do parágrafo";
 const botoesNoShadowDOM = Array.from(shadowHospedeiro.shadowRoot.querySelectorAll('button'));
 const paragrafosNoShadowDOM = Array.from(shadowHospedeiro.shadowRoot.querySelectorAll('p'));
 
-botoesNoShadowDOM.forEach(botao => botao.addEventListener('click', () => {
-    paragrafosNoShadowDOM.forEach(paragrafo => 
+botoesNoShadowDOM.forEach(botao => botao.addEventListener('click', () => {  
+  paragrafosNoShadowDOM.forEach(paragrafo => 
         {
-            
+          if (paragrafo.style.fontSize === "2rem") {
+            paragrafo.style.fontSize = "1rem"; 
+            botao.innerText = "Aumentar tamanho de fonte do parágrafo"
+          } else {
+            paragrafo.style.fontSize = "2rem"; 
+            botao.innerText = "Diminuir tamanho de fonte do parágrafo";
+          }
         })
 }))
 
 //CSS no Shadow DOM
+let folhaDeEstilo = new CSSStyleSheet();
+folhaDeEstilo.replaceSync("button { border: none; border-radius: 1rem; background-color: orange; padding: 1rem .5rem; cursor: pointer}")
+
+shadowRaiz.adoptedStyleSheets = [folhaDeEstilo];
 
 //Criação de tag customizada
-
 class ComponenteCustomizado extends HTMLElement {
     constructor() {
       super();
     }
+    
     
 
 }
