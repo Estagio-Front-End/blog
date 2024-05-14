49bc9eacb9d9e0046f3752d30dfcf885a6ea0b9e
Camila Matias <camila.matias@zappts.com.br>
2024-05-14 12:11:51 -0300

    feat: adicionar evento de mudança de background do header

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-
+window.addEventListener('scroll', () => {
+    (window.scrollY > 0) 
+    ? document.querySelector("header").style.backgroundColor = "#1F2041" 
+    : document.querySelector("header").style.backgroundColor = "rgba(18, 19, 38, 0.5)"
+});
 
