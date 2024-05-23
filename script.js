b9779fd9fa173bafdc8f8d6e2b4503f1a3f666cf
Camila Matias <camila.matias@zappts.com.br>
2024-05-23 12:22:23 -0300

    feat: adicionar estrutura e lógica do carrossel

--- script.js
+++ script.js
@@ -113,3 +113,58 @@ function gridAutoFlow(value){
     containerGridAutoFlow.style.gridAutoFlow = value;
 }
 
+//carrossel
+
+let container = document.querySelector(".container");
+let innerContainer = document.querySelector(".inner-container");
+
+if (container !== null) {
+    let pressed = false;
+    let startX;
+    let x;
+    
+    container.addEventListener("mousedown", (e) => {
+        pressed = true;
+        startX = e.offsetX - innerContainer.offsetLeft;
+        container.style.cursor = "grabbing";
+    });
+    
+    container.addEventListener("mouseenter", () => {
+        container.style.cursor = "grab";
+    });
+    
+    container.addEventListener("mouseup", () => {
+        container.style.cursor = "grab";
+        pressed = false;
+    });
+    
+    container.addEventListener("mousemove", (e) => {
+        if (!pressed) return;
+        e.preventDefault();
+    
+        x = e.offsetX;
+        innerContainer.style.left = `${x - startX}px`;
+    });
+
+    let boundItems = () => {
+        let outer = container.getBoundingClientRect();
+        let inner = innerContainer.getBoundingClientRect();
+    
+        if (parseInt(innerContainer.style.left) > 0) {
+        innerContainer.style.left = "0px";
+        }
+    
+        if (inner.right < outer.right) {
+        innerContainer.style.left = `-${inner.width - outer.width}px`;
+        }
+    };
+
+    container.addEventListener("mousemove", (e) => {
+        if (!pressed) return;
+        e.preventDefault();
+    
+        x = e.offsetX;
+        innerContainer.style.left = `${x - startX}px`;
+        boundItems();
+    });
+}
